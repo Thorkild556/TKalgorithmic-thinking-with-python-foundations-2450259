2bdbfdc6ec4cc20e97afe130117f45c4025b5b7b
{Thorkild556} <thorkild1234@gmail.com>
2023-10-31 13:29:58 +0100

    Simpler.

--- 05_02/change_stub.py
+++ 05_02/change_stub.py
@@ -19,7 +19,6 @@ def make_change(target_amount):
                 break
     return end_money
 
-
 print(make_change(24))  # 3: 20p + 2p + 2p
 print(make_change(163))  # 5: £1 + 50p + 10p + 2p + 1p
-print(make_change(201))
+print(make_change(568))
