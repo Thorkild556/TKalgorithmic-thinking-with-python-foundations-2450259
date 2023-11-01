9fa10a327e584a616020d43ab549d36cf8220dcd
Thorkild556 <thorkild1234@gmail.com>
2023-11-01 16:39:16 +0100

    binary search

--- 06_04/binary_search.py
+++ 06_04/binary_search.py
@@ -1,8 +1,26 @@
 import random
 
 
+
+
+
 def binary_search(data, target):
-    pass
+    data.sort()
+    low = 0
+    high = len(data) - 1
+    if target == data[high]:
+        return high
+    elif target == data[low]:
+        return low
+    while low <= high:
+        half = low + high // 2
+        if target == data[half]:
+            return half
+        elif target < data[half]:
+            high = half
+        elif target > data[half]:
+            low = half
+
 
 
 n = 10
@@ -10,7 +28,7 @@ max_val = 100
 data = [random.randint(1, max_val) for i in range(n)]
 data.sort()
 print("Data:", data)
-target = int(input("Enter target value: "))
+target = data[4]
 target_pos = binary_search(data, target)
 if target_pos == -1:
     print("Your target value is not in the list.")
